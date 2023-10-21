308c243c734e77928b042253823fb5e3a5db91ba
Diego Sousa <me@diego.dev.br>
2023-10-21 00:02:38 -0300

    send project

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,50 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../service/consulta-cep.service";
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(
+      private router: Router,
+      private consultaCepService: ConsultaCepService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  cadastrar(form: NgForm){
+    if(form.valid) {
+      this.router.navigate(['./sucesso'])
+    } else {
+      alert('Formulário Inválido!')
+    }
+  }
+
+  consultaCep(event: any, formCadastro: NgForm) {
+    const cep = event.target.value;
+
+    if(cep !== "") {
+      this.consultaCepService.getConsultaCep(cep).subscribe(response => {
+        this.populaEndereco(response, formCadastro);
+        console.log(response)
+      })
+    }
+  }
+
+  populaEndereco(dados: any, formCadastro: NgForm) {
+    formCadastro.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+  }
+
+}
